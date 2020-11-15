6143f1fe4660ea75031103750c7cc1d0bc0b1395
timapple-ai <58660268+timapple-ai@users.noreply.github.com>
2020-11-15 21:00:48 +0900

    refactoring

--- collapse_simulater.py
+++ collapse_simulater.py
@@ -1,19 +1,19 @@
 import csv
 import numpy as np
 
-length = 100
-atoms = np.ones(length, dtype=np.int)
+num_atoms = 100
+atoms = np.ones(num_atoms, dtype=np.int)
 
 def log_result(array, time):
     with open('./reslt.csv', 'a') as f:
         writer = csv.writer(f)
         nmsg = "{} 回目".format(time)
-        pmsg = "{} %".format(cal_ncoll_rate(array))
+        pmsg = "{} %".format(cal_uncoll_rate(array))
         result_row = np.hstack((nmsg, pmsg, array))
         writer.writerow(result_row)
 
 
-def prob_collapse(rate):
+def collapse_prob(rate):
 
     nums = np.arange(1, 1001, dtype=np.int)
     selected = np.random.choice(nums, 1)[0]
@@ -22,7 +22,7 @@ def prob_collapse(rate):
     else:
         return 0
 
-def cal_ncoll_rate(array):
+def cal_uncoll_rate(array):
     length = len(array)
     not_coll_num = array.sum()
 
@@ -30,31 +30,31 @@ def cal_ncoll_rate(array):
 
     return not_coll_rate
 
-def judg_end(array, rate):
+def is_end(array, rate):
 
-    not_coll_rate = cal_ncoll_rate(array)
+    uncoll_rate = cal_uncoll_rate(array)
 
-    if not_coll_rate < rate:
+    if uncoll_rate < rate:
         return True
     else:
         return False
 
-num_times = 1
+times = 1
 
 while True:
 
-    for i in range(length):
+    for i in range(num_atoms):
 
         if atoms[i] == 1:
-            atoms[i] = prob_collapse(100)
+            atoms[i] = collapse_prob(100)
         else:
             pass
 
-    log_result(atoms, num_times)
+    log_result(atoms, times)
 
-    num_times += 1
+    times += 1
 
-    if judg_end(atoms, 50):
+    if is_end(atoms, 50):
         break
     else:
         pass
