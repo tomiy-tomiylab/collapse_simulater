a0c5b2728d783cb5097c14834602af457b9f2a01
timapple-ai <58660268+timapple-ai@users.noreply.github.com>
2020-11-17 23:20:14 +0900

    add function: show simulate parameters

--- graph.py
+++ graph.py
@@ -9,15 +9,24 @@ y = np.array([])
 
 with open('./to_graph.csv') as f:
     reader = csv.reader(f)
-    for row in reader:
+    readed = [row for row in reader]
+    msg_f = readed[0]
+    del readed[0]
+    msg_e = readed[-1]
+    del readed[-1]
+    for row in readed:
         x = np.append(x, row[0].replace(' 回目', ''))
         y = np.append(y, row[1].replace(' %', ''))
 
 length = len(x)
+print(msg_f)
+print(msg_e)
+'''
 x = np.delete(x, length - 1)
 y = np.delete(y, length - 1)
 x = np.delete(x, 0)
 y = np.delete(y, 0)
+'''
 # x = np.flipud(x)
 # y = np.flipud(y)
 
