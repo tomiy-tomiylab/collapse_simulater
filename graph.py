5ae40627a9a785164cd5c3707132692e46c9d769
timapple-ai <58660268+timapple-ai@users.noreply.github.com>
2020-11-19 23:25:57 +0900

    fixing bug

--- graph.py
+++ graph.py
@@ -1,8 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
+import pandas as pd
 
 ax, fig = plt.subplots()
+
+fig.set_ylim(0, 100)
+# start, end = fig.get_ylim()
+# stepsize = 25
+# fig.yaxis.set_ticks(pd.np.arange(start, end, stepsize))
 
 x = np.array([])
 y = np.array([])
@@ -22,8 +28,15 @@ length = len(x)
 print(msg_f)
 print(msg_e)
 
+print("length x = {}".format(len(x)))
+print("length y = {}".format(len(y)))
+print("x max = {}".format(x[np.argmax(x)]))
+print("x min = {}".format(x[np.argmin(x)]))
+print("y max = {}".format(y[np.argmax(y)]))
+print("y min = {}".format(y[np.argmin(y)]))
+
 # x = np.flipud(x)
 # y = np.flipud(y)
 
-plt.plot(x, y, linewidth=1, color="red")
+fig.plot(x, y, linewidth=1, color="red")
 plt.show()
