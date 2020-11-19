d8bddd4be71de5082b49da14ab73008448524310
timapple-ai <58660268+timapple-ai@users.noreply.github.com>
2020-11-19 23:46:25 +0900

    fixed bug

--- graph.py
+++ graph.py
@@ -1,17 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
-import pandas as pd
 
 ax, fig = plt.subplots()
 
-fig.set_ylim(0, 100)
+# fig.set_ylim(0, 100)
 # start, end = fig.get_ylim()
 # stepsize = 25
 # fig.yaxis.set_ticks(pd.np.arange(start, end, stepsize))
 
-x = np.array([])
-y = np.array([])
+x = np.array([], dtype=np.int)
+y = np.array([], dtype=np.float32
+)
 
 with open('./to_graph.csv') as f:
     reader = csv.reader(f)
@@ -23,6 +23,9 @@ with open('./to_graph.csv') as f:
     for row in readed:
         x = np.append(x, row[0].replace(' 回目', ''))
         y = np.append(y, row[1].replace(' %', ''))
+
+x = x.astype(np.int)
+y = y.astype(np.float32)
 
 length = len(x)
 print(msg_f)
